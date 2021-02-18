6298d6e318b060150856c400e7c0670eb55ad4ff
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-18 17:26:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210216225215) do
+ActiveRecord::Schema.define(version: 20201103200552) do
 
   create_table "school_classes", force: :cascade do |t|
     t.string  "title"
